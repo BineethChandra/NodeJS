e98c5772dfabe9bda0c4746b1928d565fffe3b78
BineethChandra <99240425+BineethChandra@users.noreply.github.com>
2022-02-08 12:34:02 +0530

    Add files via upload

---
+++ models/tasks-model.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const TaskSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    require: [true, "must provide name!"],
+    trim: true,
+    maxlength: [20, "name cannot be more than 20 characters!"],
+  },
+  completed: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const tasks = mongoose.model("Task", TaskSchema);
+module.exports = tasks;
